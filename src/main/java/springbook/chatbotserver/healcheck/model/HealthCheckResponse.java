55e32c42899dd6491ccddcd948222e867bf22f45
minjin <sajajin115@gmail.com>
2025-06-02 18:35:26 +0900

    [REFACTOR] HealthChecker 인터페이스 도입으로 헬스체크 클래스 전략화

--- src/main/java/springbook/chatbotserver/healcheck/model/HealthCheckResponse.java
+++ src/main/java/springbook/chatbotserver/healcheck/model/HealthCheckResponse.java
@@ -1,4 +1,8 @@
 package springbook.chatbotserver.healcheck.model;
 
+/**
+ * 서버의 헬스 체크 응답을 나타내는 DTO 클래스입니다.
+ * @param status
+ */
 public record HealthCheckResponse(HealthStatus status) {
 }
--- src/main/java/springbook/chatbotserver/healcheck/service/MariaHealthChecker.java
+++ src/main/java/springbook/chatbotserver/healcheck/service/MariaHealthChecker.java
@@ -7,14 +7,20 @@ import org.springframework.stereotype.Service;
 import springbook.chatbotserver.healcheck.model.HealthCheckResponse;
 import springbook.chatbotserver.healcheck.model.HealthStatus;
 
+/**
+ * MariaHealthChecker는 MariaDB 데이터베이스의 건강 상태를 확인하는 서비스입니다.
+ * 데이터베이스 연결을 통해 상태를 체크하고, 결과를 HealthCheckResponse로 반환합니다.
+ */
 @Service
-public class MariaHealthChecker {
+public class MariaHealthChecker implements HealthChecker {
 
   private final DataSource dataSource;
 
   public MariaHealthChecker(DataSource dataSource) {
     this.dataSource = dataSource;
   }
+
+  @Override
   public HealthCheckResponse checkHealth() {
     try (var connection = dataSource.getConnection()) {
       if (connection.isValid(2)) {
@@ -25,5 +31,10 @@ public class MariaHealthChecker {
     } catch (Exception e) {
       return new HealthCheckResponse(HealthStatus.DOWN);
     }
+  }
+
+  @Override
+  public String target() {
+    return "MariaDB";
   }
 }
