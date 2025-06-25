0afe2b4e40919ca89d4d1172982fa1a8b60cb5f5
minjin <sajajin115@gmail.com>
2025-06-25 20:26:54 +0900

    [refactor] 단일 헬스체크 기능 삭제

--- src/main/java/springbook/chatbotserver/healcheck/controller/HealthCheckController.java
+++ src/main/java/springbook/chatbotserver/healcheck/controller/HealthCheckController.java
@@ -4,10 +4,8 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +13,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import springbook.chatbotserver.healcheck.model.HealthCheckResponse;
-import springbook.chatbotserver.healcheck.model.HealthStatus;
 import springbook.chatbotserver.healcheck.service.HealthChecker;
 
 @RestController
@@ -35,21 +32,4 @@ public class HealthCheckController {
     }
     return ResponseEntity.ok(result);
   }
-
-  @GetMapping("/{target}")
-  @Operation(summary = "단일 헬스체크", description = "서비스의 가용성을 확인합니다. 각 구성 요소의 상태를 반환합니다.(ex: Spring, Mongodb, Rasa, Mariadb 등)")
-  public ResponseEntity<HealthCheckResponse> checkTarget(@PathVariable String target) {
-    return healthCheckers.stream()
-        .filter(c -> c.target().equalsIgnoreCase(target))
-        .findFirst()
-        .map(c -> {
-          HealthCheckResponse res = c.checkHealth();
-          HttpStatus status = res.status() == HealthStatus.UP ? HttpStatus.OK : HttpStatus.INTERNAL_SERVER_ERROR;
-          return ResponseEntity.status(status).body(res);
-        })
-        .orElse(ResponseEntity
-            .status(HttpStatus.NOT_FOUND)
-            .body(new HealthCheckResponse(HealthStatus.DOWN)));
-  }
-
 }
